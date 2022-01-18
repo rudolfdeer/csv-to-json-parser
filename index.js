9929e856cced0043ff810b106204a339a9e74758
Toma Lichko <tomalichko@gmail.com>
2022-01-18 15:31:20 +0300

    feat: add transform stream

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const { program } = require('commander');
-const { pipeline } = require('stream');
-import { parse } from 'csv-parse';
-import { transformer } from './src/transformer.js';
+import fs from 'fs';
+import { program } from 'commander';
+import { pipeline } from 'stream';
+import { parse } from'csv-parse';
+//import { transformer } from './src/transformer.js';
+import { transform } from 'stream-transform';
 
 program
   .option('--sourceFile <file>', 'source file')
@@ -13,17 +13,25 @@ program
 
 const { sourceFile, resultFile, separator } = program.opts();
 
-const records = [];
-
 const parser = parse({
   delimiter: separator,
 });
 
+const transformer = transform((record, callback) => {
+  setTimeout(() => {
+    callback(null, record.join(' ')+'\n');
+  }, 500);
+}, {
+  parallel: 5
+});
+
 const inputStream = sourceFile ? fs.createReadStream(sourceFile) : process.stdin;
-const transformStream = transformer(separator);
+//const transformStream = transformer(separator);
 const outputStream = resultFile ? fs.createWriteStream(resultFile) : process.stdout;
 
-pipeline(inputStream, transformStream, outputStream, (err) => {
+
+
+pipeline(inputStream, parser, transformer, outputStream, (err) => {
   if (err) {
     process.stderr.write(`pipeline error: ${err}\n`);
     process.exit(1);
--- src/transformer.js
+++ src/transformer.js
@@ -1,17 +1,40 @@
 import { Transform } from 'stream';
+import { parse } from'csv-parse';
+// import { transform } from 'stream-transform';
 
 class Transformer extends Transform {
   constructor(separator) {
     super();
     this.separator = separator;
+    // this.parser = parse({
+    //   delimiter: this.separator,
+    // })
   }
 
   _transform(chunk, encoding, callback) {
+    //const inputString = chunk.toString();
+    // const outputString = inputString.split('\n');
+    const outputString = transform((record, cb) => {
+      setTimeout(() => {
+        callback(null, record.join(' ')+'\n');
+      }, 500);
+    }, {
+      parallel: 5
+    });
+    this.push(outputString);
     callback();
   }
 }
 
 const transformer = (separator) =>
   new Transformer(separator);
+
+
+
+// const transformer = transform((record, cb) => {
+//   cb(null, record.join(' ') + '\n');
+// }, {
+//   parallel: 5,
+// } )
 
 export { transformer };
