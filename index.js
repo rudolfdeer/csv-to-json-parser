791435fb62885a7611eab2a6e4a3b8dc83ba061c
rudolfdeer <tomalichko@gmail.com>
2022-01-19 11:03:11 +0300

    feat: implement functionality

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import fs from 'fs';
 import { program } from 'commander';
-import { pipeline } from 'stream';
-import { parse } from'csv-parse';
-//import { transformer } from './src/transformer.js';
-import { transform } from 'stream-transform';
+import csv from 'csv-parser';
 
 program
   .option('--sourceFile <file>', 'source file')
@@ -13,27 +10,20 @@ program
 
 const { sourceFile, resultFile, separator } = program.opts();
 
-const parser = parse({
-  delimiter: separator,
-});
+const obj = [];
 
-const transformer = transform((record, callback) => {
-  setTimeout(() => {
-    callback(null, record.join(' ')+'\n');
-  }, 500);
-}, {
-  parallel: 5
-});
-
-const inputStream = sourceFile ? fs.createReadStream(sourceFile) : process.stdin;
-//const transformStream = transformer(separator);
-const outputStream = resultFile ? fs.createWriteStream(resultFile) : process.stdout;
-
-
-
-pipeline(inputStream, parser, transformer, outputStream, (err) => {
-  if (err) {
-    process.stderr.write(`pipeline error: ${err}\n`);
+fs.createReadStream(sourceFile)
+  .pipe(csv())
+  .on('error', (err) => {
+    process.stderr.write(`error: ${err}\n`);
     process.exit(1);
-  }
-});
+  })
+  .on('data', (chunk) => {
+    obj.push(chunk);
+  })
+  .on('end', () => {
+    fs.writeFileSync(resultFile, JSON.stringify(obj), (err) => {
+      if (err) throw err;
+    });
+    process.exit();
+  });
